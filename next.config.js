a6512e93e09cecd9788e54b33940a52363c75a24
EmaSuriano <emanuel.suriano@gmail.com>
2020-10-22 20:14:38 +0200

    using values from package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const { name, homepage } = require('./package.json');
+
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  assetPrefix: isProd ? '/next-gh-pages-test' : '',
-  basePath: isProd ? '/next-gh-pages-test' : '',
+  assetPrefix: isProd ? homepage : '',
+  basePath: isProd ? `/${name}` : '',
 };
